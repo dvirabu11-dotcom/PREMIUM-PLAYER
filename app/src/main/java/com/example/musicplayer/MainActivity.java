5a4f26f88b12cec578cfd9c972636de85a2dc908
dvirabu11-dotcom <dvir.abu11@gmail.com>
2026-05-12 22:37:51 +0300

    chore: Update build tools and dependencies
    Updates Android build SDK version to 30 and adjusts Gradle build settings. Also refactors GitHub Actions workflow to use a newer Gradle setup action and command.

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -13,6 +13,7 @@ import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
